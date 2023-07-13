61c0b8e843e665a51aa1f8cb44ca527790b6ceac
Edwin Samuel Koh Rodriguez <edwin.koh@dineroinmediato.com.mx>
2023-07-13 12:20:59 -0600

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
-const { PORT } = require('./confgit ig')
+const { PORT } = require('./config')
 
 app.use(cors());
 app.use(express.json({ limit: '5mb' }));
