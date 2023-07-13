733e9b8ef59017a0364d9702d209eb7559e8d244
Edwin Samuel Koh Rodriguez <edwin.koh@dineroinmediato.com.mx>
2023-07-13 12:31:24 -0600

    commit

--- config.db.js
+++ config.db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 require('dotenv').config();
-const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = require('./config.js')
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = require('./config')
 
 
 const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
